ec1ceb437b03795c2dde27bb1573a9f6bf9739ac
JakDab <bloomo.private@gmail.com>
2022-11-15 14:23:07 +0100

    initial git commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+import { NextPageWithLayout } from '~/types/common.types'
+
+import { Fragment } from 'react'
+
+
+const Home: NextPageWithLayout = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>DaftAcademy – WebApp 2022</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        <h1>DaftAcademy – WebApp 2022</h1>
+      </div>
+
+    </Fragment>
+  )
+}
+
+Home.getLayout = (page) => {
+  return <div>{page}</div>;
+};
+
+export default Home
